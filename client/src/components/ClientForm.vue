0bc008af486a82800859d1853968f550ef640d5a
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-02 14:02:52 +0100

    client components

---
+++ client/src/components/ClientForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form id="clients-form">
+    <h2>Add a Booking</h2>
+    <div>
+        <label for="client-name">Client Name:</label>
+        <input type="text" id="client-name" required>
+    </div>
+    <div>
+        <label for="client-email">Client Name:</label>
+        <input type="text" id="client-email" required>
+    </div>
+    <div>
+        <label for="checked-in">Client Name:</label>
+        <input type="checkbox" id="checked-in" required>
+    </div>
+    <input type="submit" value="Book" id="book"/>
+</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+   <h1>Hotel Cat-y-dog-ia</h1>
+   <h3>You can check-in any time you like, your pets will never want to leave</h3>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
