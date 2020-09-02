a027e2ad5e208914f1a9c972ff2503ff3e025930
Timothy Saudners <timsaunders88@gmail.com>
2020-09-02 15:46:38 +0100

    added routes for index, post, delete and update

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const parser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const cors = require('cors');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
-const createRouter = require('./helpers/create_router.js')
+const createRouter = require('./helpers/create_router.js');
 
-const app = express()
-app.use(cors)
-app.use(parser)
+const app = express();
+app.use(cors());
+app.use(parser.json());
 
 MongoClient.connect('mongodb://localhost:27017')
 .then((client) => {
-  const db = client.db('hotel_clients')
-  const collection = db.collection('bookings')
-  const bookingsRouter = createRouter(collection)
-  app.use('/api/bookings', bookingsRouter)
+  const db = client.db('hotel_clients');
+  const collection = db.collection('bookings');
+  const bookingsRouter = createRouter(collection);
+  app.use('/api/bookings', bookingsRouter);
 })
-.catch(console.error)
+.catch(console.error);
 
-app.listen(3000, () => {
-  console.log(`Listening on port ${ this.address().port }`)
-})
+
+app.listen(3000, function () {
+    console.log(`Develop branch Listening on port ${ this.address().port }`);
+  });
